3c8d36f09f61630e8615409ced624946eccb26b4
JunKang <xenoficx69420@outlook.sg>
2025-01-02 10:23:22 +0800

    c++ assignment 1

---
+++ csci251_a1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main(){
+
+    string x;
+
+    cout << "Please enter config filename : ";
+    cin >> x;
+    
+    return 0;
+}
